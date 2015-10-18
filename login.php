a84be6ad3b1bd68f9ddd59d8a795be6999fdf831
Peter Ye <yechenchao@gmail.com>
2015-10-18 23:11:27 +1100

    Login page updated
    Update bootstrap style on login page

--- login.php
+++ login.php
@@ -1,61 +1,35 @@
 <!-- This page is created for user login-->
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
-	  	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
-	 	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-	  	<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-	  	<link rel="stylesheet" href="/resources/demos/style.css">
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+
+	    <title>WHAP Query System</title>
 
 	  	<!-- Bootstrap core CSS -->
 	    <link href="bootstrap-3.3.5/css/bootstrap.min.css" rel="stylesheet">
 	    <!-- Bootstrap theme -->
 	    <link href="bootstrap-3.3.5/css/bootstrap-theme.min.css" rel="stylesheet">
-	    <link href="css/theme.css" rel"stylesheet">
-
-
-	  <script>
-	  $(function() {
-	    $( "#dialog" ).dialog({
-	    	width:1000
-	    });
-	    
-	  });
-	  </script>
+	    <link href="css/theme.css" rel="stylesheet">
 	</head>
 	<body>
-		<table width="350" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-			<tr>
-				<form name="form1" method="post" action="login_authentication.php">
-					<td>
-					<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-						<tr>
-							<td colspan="3"><strong>Welcome, please login to WHAP Query System</strong></td>
-						</tr>
-						<tr>
-							<td width="78">Username</td>
-							<td width="6">:</td>
-							<td width="294"><input name="username" type="text" id="username"></td>
-						</tr>
-						<tr>
-							<td>Password</td>
-							<td>:</td>
-							<td><input name="password" type="password" id="password"></td>
-						</tr>
-						<tr>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-							<td><input type="submit" name="Submit" class="btn btn-primary" value="Login"></td>
-						</tr>
-						<td>
-					</table>
-					</td>
-				</form>
-			</tr>
-		</table>
 
-		<div id="dialog" title="Basic dialog">
-  			<p>This website was created for the Women Health Aging Project(WHAP) of the University of Melbourne, Medical Department. It provides a convinient interface for researchers to access and retrieve thousands of research data records.</p>
-			<p>You could enter admin/admin to login to this sample site.</p>
+		<div class="alert alert-success" role="alert">
+			<p>Login username and password are both 'admin'<p>
+		</div>
+		<div class="container">
+			<form class="form-signin" name="form1" method="post" action="login_authentication.php">
+			<h2 class="form-signin-heading">Welcome, please sign in</h2>
+				<label for="username" class="sr-only">Username</label>
+				<input name="username" type="text" id="username" class="form-control" placeholder="Username" required autofocus>
+				<input name="password" type="password" id="password" class="form-control" placeholder="Password" required>
+				<button type="submit" class="btn btn-lg btn-block btn-primary">Sign in</button>
+			</form>
 		</div>
 
 	</body>
